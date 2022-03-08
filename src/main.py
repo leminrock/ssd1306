f34b9bcddf1cc71f938f673ea3cd2dc261a0e554
franeum <elettrochuck@gmail.com>
2022-03-08 18:24:08 +0100

    updated

--- src/main.py
+++ src/main.py
@@ -25,22 +25,24 @@ current_page = shortpage
 
 def button_routine(gpio):
     global current_page
-    print("push number:", gpio)
-    sel = current_page.get_selected()
+    if gpio == PIN3:
+        sel = current_page.get_selected()
 
-    if not Graph.is_leaf(sel):
-        print("not leaf")
-        nodes = Graph.get_nodes(sel)
+        if not Graph.is_leaf(sel):
+            print("not leaf")
+            nodes = Graph.get_nodes(sel)
 
-        if len(nodes) <= SHORT_LONG:
-            current_page = shortpage
+            if len(nodes) <= SHORT_LONG:
+                current_page = shortpage
+            else:
+                print("pagelong")
+                current_page = longpage
+
+            current_page.populate(nodes)
         else:
-            print("pagelong")
-            current_page = longpage
-
-        current_page.populate(nodes)
+            print("leaf")
     else:
-        print("leaf")
+        print("GO BACK")
 
 
 if __name__ == '__main__':
