6d320dad121ec3ed72fde37840a5eca619ffe472
franeum <elettrochuck@gmail.com>
2022-03-10 22:11:58 +0100

    set patcher module

--- src/patcher/patch.py
+++ src/patcher/patch.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
 
-import subprocess as sp
+import sys
+import subprocess as sproc
 from pathlib import Path
 
-diry = '.'
-path = Path(diry).resolve()
-path = path / Path('test.sh')
+COMMAND = "puredata -nogui -jack"
+SERVICESCRIPT = "setpatch.sh"
+SHEBANG = "#!/usr/bin/env bash\n\n"
+SYSTEMD_COMMAND = "systemctl"
 
 
-with open(path, 'w') as f:
-    f.write("#!/usr/bin/env bash\n")
-    f.write("echo ciao\n")
+def _set(patchname):
+    """set puredata patch to execute immediately and at boot
 
-sp.run(str(path))
+    :param patchname: puredata file (with complete path)
+    :type patchname: pathlib.PosixPath
+    """
+    path = str(patchname.resolve())
+    script = Path(SERVICESCRIPT).resolve()
+
+    with open(str(script), 'w') as f:
+        f.write(SHEBANG)
+        f.write(f"{COMMAND} {path}")
+
+    script.chmod(0o755)
+
+
+def _service_restart(service):
+    sproc.run([SYSTEMD_COMMAND, 'restart', service])
+
+
+def set_patch(patchname, jackdservice, pdservice):
+    _set(patchname)
+    _service_restart(jackdservice)
+    _service_restart(pdservice)
+    sys.exit(0)
---
+++ src/patcher/setpatch.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+puredata -nogui -jack /home/odisseo/Documenti/leminrock/ssd1306/src/patcher/lapislazzulo.pd
