0afc738b880d6b10878e0461137867565b12de71
franeum <elettrochuck@gmail.com>
2022-03-22 17:01:24 +0100

    updated

--- src/main2.py
+++ src/main2.py
@@ -6,17 +6,16 @@ from soft import menu_builder as mb
 
 def test_routine(item):
     print("cedo il comando")
-    item.isr_exit()
     CURRENT = item.children[0]
     CURRENT.routine(item._pin)
+    item.isr_exit()
 
 
 mb.MAINMENU.routine(test_routine, 10)
-# mb.MAINMENU.loop()
 
 CURRENT = mb.MAINMENU
 
 
-while True():
+while True:
     print(CURRENT.name)
     time.sleep(0.5)
