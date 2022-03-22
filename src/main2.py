a50e95569f4c8458206b8c11d21cab595fb39edd
franeum <elettrochuck@gmail.com>
2022-03-22 17:38:20 +0100

    updated

--- src/main2.py
+++ src/main2.py
@@ -21,7 +21,7 @@ while True:
     if PREVIOUS:
         print("previous:", PREVIOUS.name)
     print(CURRENT.name)
-    if PREVIOUS != CURRENT:
+    if PREVIOUS and PREVIOUS != CURRENT:
         PREVIOUS.isr_exit()
         CURRENT.routine(test_routine, 10)
     time.sleep(0.5)
