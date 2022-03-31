1facaef60a3873cb1fde8fa8f6c395f33e229a48
franeum desk <elettrochuck@gmail.com>
2022-03-31 16:35:53 +0200

    updated hooks

--- hooks/sethooks.py
+++ hooks/sethooks.py
@@ -8,5 +8,16 @@ source = p / Path('../.git/hooks')
 commit_file = Path('commit-msg')
 merge_file = Path('post-merge')
 
-(source / commit_file).symlink_to((p / commit_file).resolve())
-(source / merge_file).symlink_to((p / merge_file).resolve())
+dest_commit = source / commit_file
+dest_merge = source / merge_file
+
+if dest_commit.is_file():
+    print(f"REMOVING EXISTING FILE {dest_commit}")
+    dest_commit.unlink()
+
+if dest_merge.is_file():
+    print(f"REMOVING EXISTING FILE {dest_merge}")
+    dest_merge.unlink()
+
+dest_commit.symlink_to((p / commit_file).resolve())
+dest_merge.symlink_to((p / merge_file).resolve())
