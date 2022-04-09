9f5aaceb45bbf2498c53fda2a373f76f8809d8a4
franeum <elettrochuck@gmail.com>
2022-04-09 17:21:54 +0200

    updated

--- source/common/cfg.py
+++ source/common/cfg.py
@@ -1,23 +1,3 @@
-class Status():
-    def __init__(self, current=None, previous=None):
-        self._current = current
-        self._previous = previous
-
-    @property
-    def current(self):
-        return self._current
-
-    @property
-    def previous(self):
-        return self._previous
-
-    @current.setter
-    def current(self, item):
-        self._current = item
-
-    @previous.setter
-    def previous(self, item):
-        self._previous = item
-
+from soft.entities import Status
 
 MAINSTATUS = Status()
--- source/soft/entities.py
+++ source/soft/entities.py
@@ -177,3 +177,25 @@ class ItemApp(Item):
 
     def draw(self):
         pass
+
+
+class Status():
+    def __init__(self, current=None, previous=None):
+        self._current = current
+        self._previous = previous
+
+    @property
+    def current(self):
+        return self._current
+
+    @property
+    def previous(self):
+        return self._previous
+
+    @current.setter
+    def current(self, item):
+        self._current = item
+
+    @previous.setter
+    def previous(self, item):
+        self._previous = item
