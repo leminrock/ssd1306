d36b960719728054352c980e07fed3fcd1c77671
franeum <elettrochuck@gmail.com>
2022-04-09 19:44:31 +0200

    updated

--- source/pages/mainmenu.py
+++ source/pages/mainmenu.py
@@ -22,8 +22,8 @@ def forward_routine(state_obj):
     if MAINSTATUS.current.children[0]:
         # MAINSTATUS.empty()
         #print(f"ha senso: {item.children[0]}")
-        MAINSTATUS.current = MAINSTATUS.current.chidren[0]
         MAINSTATUS.previous = MAINSTATUS.current
+        MAINSTATUS.current = copy.deepcopy(MAINSTATUS.current.chidren[0])
         log.info(f"previous: {MAINSTATUS.previous}")
         log.info(f"current: {MAINSTATUS.current}")
     else:
