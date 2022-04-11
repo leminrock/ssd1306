e740fee1d2f19414352a82ea38bf07a89710dac0
franeum <elettrochuck@gmail.com>
2022-04-11 18:29:31 +0200

    updated

--- source/pages/patch.py
+++ source/pages/patch.py
@@ -11,18 +11,22 @@ log.config(__name__)
 
 PATCHESPATH = Path('../patches').resolve()
 
+def itempatch_forward_routine(arg):
+    pass
+
 PATCH = ItemMenu('PATCH')
 PATCH.register_right_routine(PIN_FORWARD, rtn.forward_routine, MAINSTATUS)
 PATCH.register_left_routine(PIN_BACKWARD, rtn.backward_routine, MAINSTATUS)
 PATCH.register_rotary(PIN_ROTARY_1, PIN_ROTARY_2)
 
 ITEMPATCH = []
-
 dirs = list(PATCHESPATH.glob('*'))
 
 for _dir in sorted(dirs):
     path = _dir / Path('main.pd')
     itempatch = ItemPatch(_dir.stem)
+    itempatch.register_right_routine(PIN_FORWARD, itempatch_forward_routine, MAINSTATUS)
+    itempatch.register_left_routine(PIN_BACKWARD, rtn.backward_routine, MAINSTATUS)
     ITEMPATCH.append(itempatch)
     #patch.set_command(set_patch, path, JACKDSERVICE, PDSERVICE)
     #Graph.one_to_one(PATCHES, patch)
